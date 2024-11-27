bbe0dcdd5e2972615ad189d8e5e1432a47fc2d53
22051988Di <vitalii.soloninko@gmail.com>
2024-11-27 17:40:19 +0100

    0.4	SideBar header layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { SidebarComponent } from './sidebar/sidebar.component'
+import { MainComponent } from './main/main.component'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, SidebarComponent, MainComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-sidebar';
+	title = 'angular-sidebar'
 }
