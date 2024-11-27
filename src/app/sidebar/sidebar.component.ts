c64edef572eb1ba9fa3bf8031a76cca41360dcc2
22051988Di <vitalii.soloninko@gmail.com>
2024-11-27 18:01:05 +0100

    0.5	Sidebar navigation layout, logic

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+	selector: 'app-sidebar',
+	standalone: true,
+	imports: [RouterModule],
+	templateUrl: './sidebar.component.html',
+	styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+	menuItems = [
+		{
+			RouterLink: 'dashboard',
+			icon: 'fal fa-home',
+			label: 'Dashboard',
+		},
+		{
+			RouterLink: 'products',
+			icon: 'fal fa-box-open',
+			label: 'Products',
+		},
+		{
+			RouterLink: 'pages',
+			icon: 'fal fa-file',
+			label: 'Pages',
+		},
+		{
+			RouterLink: 'settings',
+			icon: 'fal fa-cog',
+			label: 'Settings',
+		},
+	]
 }
