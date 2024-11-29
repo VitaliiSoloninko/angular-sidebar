e5801cca4e302cd2a862db7971f642d3bd49ff30
22051988Di <vitalii.soloninko@gmail.com>
2024-11-29 14:25:07 +0100

    2.3	Fix sidebar when refresh mobile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, signal } from '@angular/core'
+import { Component, HostListener, OnInit, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { MainComponent } from './main/main.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
@@ -10,7 +10,7 @@ import { SidebarComponent } from './sidebar/sidebar.component'
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	isSidebarSmall = signal<boolean>(false)
 	screenWidth = signal<number>(window.innerWidth)
 
@@ -20,6 +20,10 @@ export class AppComponent {
 		if (this.screenWidth() < 768) {
 			this.isSidebarSmall.set(true)
 		}
+	}
+
+	ngOnInit(): void {
+		this.isSidebarSmall.set(this.screenWidth() < 768)
 	}
 
 	changeSidebar(isSidebarSmall: boolean): void {
