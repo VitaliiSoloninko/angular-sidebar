22ccade3db0b14c05421187d31bb915cc4d47551
22051988Di <vitalii.soloninko@gmail.com>
2024-11-29 14:18:14 +0100

    2.2	Main auto resize on 768 mobile device

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Component, computed, input } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
 	selector: 'app-main',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, CommonModule],
 	templateUrl: './main.component.html',
 	styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+	isSidebarSmall = input.required<boolean>()
+	screenWidth = input.required<number>()
+
+	sizeClass = computed(() => {
+		const isSidebarSmall = this.isSidebarSmall()
+		if (isSidebarSmall) {
+			return ''
+		}
+		return this.screenWidth() > 768 ? 'body-mobile' : 'body-pc'
+	})
+}
