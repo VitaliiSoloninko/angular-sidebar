613dfe109657525e4407146c31efa9e13ff962c3
22051988Di <vitalii.soloninko@gmail.com>
2024-11-29 13:45:28 +0100

    2.1	Sidebar - auto resize 768px with host licenser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core'
+import { Component, HostListener, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { MainComponent } from './main/main.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
@@ -12,6 +12,15 @@ import { SidebarComponent } from './sidebar/sidebar.component'
 })
 export class AppComponent {
 	isSidebarSmall = signal<boolean>(false)
+	screenWidth = signal<number>(window.innerWidth)
+
+	@HostListener('window:resize')
+	onResize() {
+		this.screenWidth.set(window.innerWidth)
+		if (this.screenWidth() < 768) {
+			this.isSidebarSmall.set(true)
+		}
+	}
 
 	changeSidebar(isSidebarSmall: boolean): void {
 		this.isSidebarSmall.set(isSidebarSmall)
